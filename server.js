5fff830b3ef130b7c9770fe6f4a62265a83b5ab8
Yangming2023 <lcheng@eastsideprep.org>
2026-02-06 12:36:58 -0800

    onkk

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,28 +7,21 @@ app.use(express.static("public"));
 
 
 app.post("/api/game", (req, res) => {
-  const db = readDB();
   db.gameState = req.body;
-  writeDB(db);
   res.json({ ok: true });
 });
 
 app.get("/api/game", (req, res) => {
-  const db = readDB();
   res.json(db.gameState);
 });
 
 app.put("/api/game", (req, res) => {
-  const db = readDB();
   db.gameState = req.body;
-  writeDB(db);
   res.json({ ok: true });
 });
 
 app.delete("/api/game", (req, res) => {
-  const db = readDB();
   db.gameState = null;
-  writeDB(db);
   res.json({ ok: true });
 });
 
